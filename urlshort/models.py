3cac20392705b158330549daa5b4c2f97dd7510b
kyoobi <vecha.sumanth@gmail.com>
2021-07-09 20:47:35 +0530

    added user feature

--- urlshort/models.py
+++ urlshort/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
 
 class ShortURL(models.Model):
     original_url = models.URLField(max_length=700)
     short_url = models.CharField(max_length=100)
     time_date_created = models.DateTimeField()
-
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     def __str__(self):
         return self.original_url
